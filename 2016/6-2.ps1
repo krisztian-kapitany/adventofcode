0b9d645819ce6288a185b410ed79982ba4f21513
krka02 <krisztian.kapitany@betssongroup.com>
2016-12-07 10:46:13 +0100

    Day 6

---
+++ 2016/6-2.ps1
@@ -0,0 +1,9 @@
+$clues = gc "clue6.txt"
+
+$result = @()
+
+for ($i = 0; $i -lt 8; $i++) {
+  $result += $clues | Group-Object -Property { $_.substring($i,1) } | Sort-Object Count | Select-Object Name -First 1
+}
+
+Write-Host "Decoded: $($result.Name -join '')"
